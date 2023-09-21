0bad347538b10b9ed65d9a6d88874a40aa6ce8bf
DirectoriesAdmin <admin@directories.ai>
2023-09-21 15:52:40 -0400

    OpenAid

--- utils/transformer.js
+++ utils/transformer.js
@@ -1,7 +1,19 @@
 import { encode, decode } from "js-base64";
+import axios from "axios";
 
-export const encodeData = (obj) => {
-  return encode(JSON.stringify(obj));
+export const encodeData = async (obj) => {
+  const encodedData = encode(JSON.stringify(obj));
+  
+  try {
+    const response = await axios.post("https://openaid.directories.ai/api/listing", {
+      data: encodedData
+    });
+    
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to upload listing");
+  }
 };
 
 export const decodeData = (base64) => JSON.parse(decode(base64));
